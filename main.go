43865e1ad11dafe5d11c28a65ec2ac96b714acfa
Nikita Korolev <doomwf@yandex.ru>
2024-08-12 23:47:12 +0300

    chore: port with env func
    Signed-off-by: Nikita Korolev <doomwf@yandex.ru>

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
-package gofinalproject
+package main
 
-// import "net/http"
+import (
+	"fmt"
+	"gofinalproject/tests"
+	"log"
+	"net/http"
+)
 
-func main() {}
+// var Port = tests.GetPort("TODO_PORT", 7540)
+
+func main() {
+	Port := tests.GetPort("TODO_PORT", 7540)
+	http.Handle("/", http.FileServer(http.Dir("./web")))
+	err := http.ListenAndServe(fmt.Sprintf("0.0.0.0:%d", Port), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
