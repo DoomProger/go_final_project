edda04c383af87146a64dff89ca3e4012a39e41f
Nikita Korolev <doomwf@yandex.ru>
2024-08-31 14:15:40 +0300

    chore: remove old code
    Signed-off-by: Nikita Korolev <doomwf@yandex.ru>

--- z_old/scheduler.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS scheduler;
-CREATE TABLE scheduler (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    date VARCHAR(8) DEFAULT "", -- handle YYYYMMDD format
-    title TEXT DEFAULT "",
-    comment TEXT DEFAULT "",
-    repeat VARCHAR(128) DEFAULT ""
-);
-CREATE INDEX idx_scheduler_date ON scheduler(date);
