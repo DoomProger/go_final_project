d652cd5770a746488daf81cae4b08be951e0b3a5
Nikita Korolev <doomwf@yandex.ru>
2024-08-31 23:56:39 +0300

    chore: add auth

--- internal/handlers/auth_handler.go
+++ internal/handlers/auth_handler.go
@@ -4,32 +4,19 @@ import (
 	"crypto/sha256"
 	"encoding/hex"
 	"encoding/json"
-	"gofinalproject/config"
 	"log"
 	"net/http"
 	"os"
-	"time"
 
 	"github.com/golang-jwt/jwt/v5"
 )
 
-var todoPasswordHash string
 var todoPassword = os.Getenv("TODO_PASSWORD")
 
-var jwtKey = []byte("SecretJWTKey_2142")
+var jwtKey []byte
 
 type Credentials struct {
 	Password string `json:"password"`
-}
-
-type Claims struct {
-	PasswordHash string `json:"password_hash"`
-	jwt.RegisteredClaims
-}
-
-func computeChecksum(str string) string {
-	hash := sha256.Sum256([]byte(str))
-	return hex.EncodeToString(hash[:])
 }
 
 func hashPassword(password string) string {
@@ -43,14 +30,10 @@ func init() {
 		log.Println("TODO_PASSWORD environment variable is not set")
 		os.Exit(1)
 	}
-	todoPasswordHash = hashPassword(todoPassword)
 }
 
 func SignIn(w http.ResponseWriter, r *http.Request) {
-	// func (th *TaskHandler) SignIn(w http.ResponseWriter, r *http.Request) {
 	var creds Credentials
-
-	log.Println("SignIn hand")
 
 	err := json.NewDecoder(r.Body).Decode(&creds)
 	if err != nil {
@@ -58,47 +41,27 @@ func SignIn(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Check the password
-	if hashPassword(creds.Password) != todoPasswordHash {
+	if creds.Password != todoPassword {
 		writeJSONError(w, http.StatusUnauthorized, "Invalid password")
 		return
 	}
 
-	// Create the JWT token
-	expirationTime := time.Now().Add(config.TokenTTL * time.Hour)
-	claims := &Claims{
-		PasswordHash: todoPasswordHash,
-		RegisteredClaims: jwt.RegisteredClaims{
-			ExpiresAt: jwt.NewNumericDate(expirationTime),
-		},
-	}
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	tokenStr, err := token.SignedString(jwtKey)
+	jwtKey = []byte(hashPassword(creds.Password))
+
+	jwtToken := jwt.New(jwt.SigningMethodHS256)
+
+	tokenStr, err := jwtToken.SignedString(jwtKey)
+
 	if err != nil {
 		http.Error(w, "Could not generate token", http.StatusInternalServerError)
 		return
 	}
 
-	// http.SetCookie(w, &http.Cookie{
-	// 	Name:     "token",
-	// 	Value:    tokenStr,
-	// 	Expires:  expirationTime,
-	// 	HttpOnly: true,
-	// })
-
-	log.Println(tokenStr, "<-- token")
-
-	// http.Redirect(w, r, "/", http.StatusSeeOther)
-
-	// json.NewEncoder(w).Encode(creds)
 	json.NewEncoder(w).Encode(map[string]string{"token": tokenStr})
 }
 
-// func (th *TaskHandler) AuthMiddleware(next http.Handler) http.Handler {
 func AuthMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-
-		log.Println("AuthMiddleware hand")
 
 		tokenCookie, err := r.Cookie("token")
 		if err != nil {
@@ -107,20 +70,16 @@ func AuthMiddleware(next http.Handler) http.Handler {
 		}
 
 		tokenStr := tokenCookie.Value
-		claims := &Claims{}
-		token, err := jwt.ParseWithClaims(tokenStr, claims, func(t *jwt.Token) (interface{}, error) {
+		jwtToken, err := jwt.Parse(tokenStr, func(t *jwt.Token) (interface{}, error) {
 			return jwtKey, nil
 		})
-
-		log.Println(tokenStr)
-
-		if err != nil || !token.Valid {
-			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		if err != nil {
+			http.Error(w, "Cannot parse token", http.StatusUnauthorized)
 			return
 		}
 
-		if claims.PasswordHash != todoPasswordHash {
-			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		if !jwtToken.Valid {
+			http.Error(w, "Authentification required", http.StatusUnauthorized)
 			return
 		}
 
